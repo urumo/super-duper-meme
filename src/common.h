e88d52c8a919a4cd9336685acf19e41ef99dd049
Aram <aramhrptn@hotmail.com>
2021-03-13 05:11:32 +0700

    initial commit

---
+++ src/common.h
@@ -0,0 +1,12 @@
+//
+// Created by aramh on 3/13/2021.
+//
+
+#ifndef CSCRIPTY_COMMON_H
+#define CSCRIPTY_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CSCRIPTY_COMMON_H
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "common.h"
+
+int main(int argc, const char *argv[]) {
+    return 0;
+}
