8654bd91acc19c00686d479233b3c8c97f9907de
Aram <aramhrptn@hotmail.com>
2021-03-13 06:45:11 +0700

    negate

--- src/debug.c
+++ src/debug.c
@@ -37,6 +37,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("cst", chunk, offset);
+        case OP_NEGATE:
+            return simpleInstruction("neg", offset);
         case OP_RETURN:
             return simpleInstruction("ret", offset);
         default: {
