427efc1dcd26e9d718eb8432d6588d2614123ed6
Aram <aramhrptn@hotmail.com>
2021-03-14 04:49:30 +0700

    lexical grammar

--- src/scanner.c
+++ src/scanner.c
@@ -24,6 +24,18 @@ static bool isAtEnd() {
     return *scanner.current == '\0';
 }
 
+static char advance() {
+    scanner.current += 1;
+    return scanner.current[-1];
+}
+
+static bool match(char expected) {
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+    scanner.current += 1;
+    return true;
+}
+
 static Token makeToken(TokenType type) {
     Token token;
     token.type = type;
@@ -45,6 +57,42 @@ static Token errorToken(const char *message) {
 Token scanToken() {
     scanner.start = scanner.current;
     if (isAtEnd()) return makeToken(T_EOF);
-
+    char c = advance();
+    switch (c) {
+        case '(':
+            return makeToken(T_LPAREN);
+        case ')':
+            return makeToken(T_RPAREN);
+        case '{':
+            return makeToken(T_LBRACE);
+        case '}':
+            return makeToken(T_RBRACE);
+        case ';':
+            return makeToken(T_SEMICOLON);
+        case ',':
+            return makeToken(T_COMMA);
+        case '.':
+            return makeToken(T_DOT);
+        case '-':
+            return makeToken(T_MINUS);
+        case '+':
+            return makeToken(T_PLUS);
+        case '/':
+            return makeToken(T_SLASH);
+        case '*':
+            return makeToken(T_ASTERISK);
+        case '!':
+            return makeToken(
+                    match('=') ? T_NE : T_BANG);
+        case '=':
+            return makeToken(
+                    match('=') ? T_EQ : T_ASSIGN);
+        case '<':
+            return makeToken(
+                    match('=') ? T_LTE : T_LT);
+        case '>':
+            return makeToken(
+                    match('=') ? T_GTE : T_GT);
+    }
     return errorToken("Unexpected Character.");
 }
